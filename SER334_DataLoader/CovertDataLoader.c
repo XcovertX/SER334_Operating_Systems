bc3cd5615a6ecb7d5d770b692900b7d81ea7adfe
James <jcovert2@asu.edu>
2022-02-23 12:31:27 -0800

    DataLoader.c base built

---
+++ SER334_DataLoader/CovertDataLoader.c
@@ -0,0 +1,30 @@
+/**
+ * Loads data files
+ *
+ * completion time 1hr
+ * @author James Covert
+ * @version 1.0
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "DataLoader.h"
+
+/**
+ * Loads a test_scenario strut from a textfile.
+ *
+ * @param filename The name of the file to load.
+ * @return A struct containing the loaded file.
+ */
+struct test_scenario* load_test_data(char* filename){
+    struct test_scenario *ts = (struct test_scenario*)malloc(sizeof(struct test_scenario));
+    FILE *file = fopen(filename, "r");
+    fscanf(file, "%d", &ts->page_count);
+    fscanf(file, "%d", &ts->frame_count);
+    ts->refstr_len = 0;
+    while(fscanf(file, "%d", &ts->refstr[ts->refstr_len]) != EOF) {
+        ts->refstr_len++;
+    }
+    return ts;
+}
+
---
+++ SER334_DataLoader/CovertPageTable.c
@@ -0,0 +1,4 @@
+//
+// Created by james on 2/23/22.
+//
+
